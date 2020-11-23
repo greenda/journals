f9b332ebd951dc03e5283d2f6bfc6cb0846f69df
greenda <a.gontareva@gmail.com>
2020-11-23 22:57:17 +0400

    Инициализация проекта

---
+++ src/JournalNavigation/JournalNavigation.svelte
@@ -0,0 +1,21 @@
+<script>
+    import NavigationItem from '../NavigationItem/NaviagtionItem.svelte';
+
+    export let articles = {};
+    export let onSelect = () => {};
+
+</script>
+
+<style>
+    .container {
+        text-align: left;
+    }
+</style>
+
+<div class="container">
+    <h4>Содержание</h4>
+    {#each articles as article }
+        <!-- TODO привязываться не к индексам, а к id -->
+        <NavigationItem {onSelect} label={`День ${article.day} - ${article.date}`} value={article.day}/>    
+    {/each}
+</div>
---
+++ src/Part/Part.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let part = '';
+</script>
+
+<style>
+    .annotation {
+        line-height: 8px;
+        font-size: 0.8em;
+        font-style: italic;
+        text-align: center;
+    }
+
+    .imageContainer {
+        padding: 0px 16px;
+        text-align: center;
+    }
+</style>
+
+<h5>{part.time}</h5>
+{#each part.paragraphes as paragraph}
+    <p>{paragraph}</p>
+{/each}
+{#if part.image}
+    <div class="imageContainer">
+        <img src={part.image.src} alt={part.image.annotation}/>
+    </div>
+    
+    <p class="annotation">{part.image.annotation}</p>
+{/if}
